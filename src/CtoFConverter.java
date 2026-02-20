476e3d480ade9109864204652b527b1da0ca61e6
Daniel Kleinman <danielkleinman70@gmail.com>
2026-02-20 10:33:17 -0500

    Initial commit

---
+++ src/CtoFConverter.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class CtoFConverter {
+    public static void main() {
+        Scanner in = new Scanner(System.in);
+        double C = 0;
+        double F = 0;
+        boolean done = false;
+        String trash = "";
+        do {
+            System.out.print("Enter number in Celcius: ");
+
+            if(in.hasNextDouble()) {
+                C = in.nextDouble();
+                in.nextLine();
+                done = true;
+            }
+        else
+            {
+                trash = in.nextLine();
+                System.out.println("You inputted :" + trash + " which is not a number. Please input a number.");
+            }
+        }while(!done);
+        F = (C * 9 / 5) + 32;
+        System.out.println("This number in fahrenheit is: " + F);
+
+    }
+}
